a12bc2e6335b921dce72742cd003d82b24a1001b
Jaeson Fan <sergefan@live.com>
2024-04-01 01:00:45 +0900

    after 7.7.5.2

--- src/startup.rs
+++ src/startup.rs
@@ -8,12 +8,17 @@ use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
 use crate::email_client::EmailClient;
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check, subscribe};
 
 pub struct Application {
     port: u16,
     server: Server,
 }
+
+// We need to define a wrapper type in order to retrieve the URL in the `subscribe` handler.
+// Retrieval from the context, in actix-web, is type-based:
+// using a raw `String` would expose us to conflicts.
+pub struct ApplicationBaseUrl(pub String);
 
 impl Application {
     pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
@@ -38,7 +43,12 @@ impl Application {
         let listener = TcpListener::bind(address)?;
 
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, connection_pool, email_client)?;
+        let server = run(
+            listener,
+            connection_pool,
+            email_client,
+            configuration.application.base_url,
+        )?;
 
         Ok(Self { port, server })
     }
@@ -61,10 +71,12 @@ pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
+    base_url: String,
 ) -> Result<Server, std::io::Error> {
     // Wrap the pool using web::Data, which boils down to an Arc smart pointer
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
+    let base_url = web::Data::new(ApplicationBaseUrl(base_url));
 
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
@@ -73,9 +85,11 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             // Register the connection as part of the application state
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
+            .app_data(base_url.clone())
     })
     .listen(listener)?
     .run();
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,7 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
+use wiremock::MockServer;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -26,7 +27,9 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 
 pub struct TestApp {
     pub address: String,
+    pub port: u16,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -46,6 +49,8 @@ pub async fn spawn_app() -> TestApp {
     // Init logging
     Lazy::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     // Randomise configuration to ensure test isolation
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration");
@@ -55,6 +60,9 @@ pub async fn spawn_app() -> TestApp {
 
         // Use a random OS port
         c.application.port = 0;
+
+        // Use the mock server as email API
+        c.email_client.base_url = email_server.uri();
 
         c
     };
@@ -66,15 +74,16 @@ pub async fn spawn_app() -> TestApp {
     let application = Application::build(configuration.clone())
         .await
         .expect("Failed to build application");
-
-    let address = format!("http://127.0.0.1:{}", application.port());
+    let application_port = application.port();
 
     // `tokio::spawn` runs the server concurrently, so it won't block the rest of the test
     tokio::spawn(application.run_until_stopped());
 
     TestApp {
-        address,
+        address: format!("http://127.0.0.1:{}", application_port),
+        port: application_port,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
