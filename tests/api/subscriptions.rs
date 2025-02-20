8f3c0d4bfad5123e9e114cbe84653c022e468226
Jaeson Fan <sergefan@live.com>
2025-02-21 04:08:43 +0800

    fix minor lint warning

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,6 +1,7 @@
-use crate::helpers::spawn_app;
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
+
+use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
@@ -134,7 +135,7 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     // Assert
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let confirmation_links = app.get_confirmation_links(&email_request);
+    let confirmation_links = app.get_confirmation_links(email_request);
 
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
